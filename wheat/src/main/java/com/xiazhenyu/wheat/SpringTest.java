97d6aac39f5a9ba0b785306556f11350434bdc7c
xiazhenyu <xiazhenyu77@aliyun.com>
2021-09-04 18:05:52 +0800

    queue

---
+++ wheat/src/main/java/com/xiazhenyu/wheat/SpringTest.java
@@ -0,0 +1,23 @@
+package com.xiazhenyu.wheat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Date: 2021/9/3
+ * <p>
+ * Description:
+ *
+ * @author xiazhenyu
+ */
+public class SpringTest {
+
+
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(5);
+        integerList.remove(Integer.valueOf(4));
+
+    }
+
+}
