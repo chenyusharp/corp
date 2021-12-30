4b02dceb95fdad5da28fcc77debd381e66cc1dcb
xiazhenyu <xiazhenyu77@aliyun.com>
2021-12-30 20:44:22 +0800

    ReverseInteger

--- wheat/src/main/java/com/xiazhenyu/leetcode/editor/cn/ReverseInteger.java
+++ wheat/src/main/java/com/xiazhenyu/leetcode/editor/cn/ReverseInteger.java
@@ -56,7 +56,7 @@ public class ReverseInteger {
         public int reverse(int x) {
             int rev = 0;
             while (x != 0) {
-                if (x < Integer.MIN_VALUE / 10 || x > Integer.MAX_VALUE / 10) {
+                if (rev < Integer.MIN_VALUE / 10 || rev > Integer.MAX_VALUE / 10) {
                     return 0;
                 }
                 int tem = x % 10;
