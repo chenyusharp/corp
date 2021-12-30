ec0ee4f467d569c3ee7157d3c3c550d76e0c4cb9
xiazhenyu <xiazhenyu77@aliyun.com>
2021-12-30 19:59:28 +0800

    jvm

---
+++ Sesame/src/main/java/com/xiazhenyu/sesame/classLoader/ClassLoader.java
@@ -0,0 +1,17 @@
+package com.xiazhenyu.sesame.classLoader;
+
+/**
+ * Date: 2021/12/28
+ * <p>
+ * Description:
+ *
+ * @author xiazhenyu
+ */
+public class ClassLoader {
+
+
+    public static void main(String[] args) {
+        System.out.println("class loading");
+    }
+
+}
