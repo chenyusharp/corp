98e2b71b7c1e65c23d97ca09c0c0a701aa85c044
xiazhenyu <xiazhenyu77@aliyun.com>
2022-04-24 23:17:07 +0800

    test

--- sesame/src/main/java/com/xiazhenyu/sesame/MyNewTest.java
+++ sesame/src/main/java/com/xiazhenyu/sesame/MyNewTest.java
@@ -2,6 +2,7 @@ package com.xiazhenyu.sesame;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Date: 2022/3/16
@@ -14,13 +15,17 @@ public class MyNewTest {
 
 
     public static void main(String[] args) {
-        List<String> list = new ArrayList<String>() {
+        /*List<String> list = new ArrayList<String>() {
             {
                 add("hello");
                 add("world");
             }
         };
-        System.out.println(list);
+        System.out.println(list);*/
+         final Pattern EXCHANGE_RATE_PATTERN = Pattern.compile("^(([1-9][0-9]{1,9}(\\.[0-9]{0,4})?)|(0\\.[0-9]?[1-9][0-9]{0,2}))$");
+
+        Pattern ss = Pattern.compile("^0(\\.[0-9]{0,4})?$");
+        System.out.println(EXCHANGE_RATE_PATTERN.matcher("11213000000.0000").find());
     }
 
 }
